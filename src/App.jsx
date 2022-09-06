4cc2017aa9e90bd347ac7b20ddf13d6636768a9d
Inna Huzhva <guzhva.inna@gmail.com>
2022-09-06 15:44:51 +0300

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Map from "./Map.jsx";
+import Data from "./Data.jsx";
+import "./normalize.css"
+import "./style.css";
+
+function App() {
+  return (
+    <>
+      <Map />
+      <Data />
+    </>
+  );
+}
+
+export default App;
