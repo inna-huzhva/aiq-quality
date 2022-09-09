05d48b31da42d790e8f844eff59ca0b7de05976c
Inna Huzhva <guzhva.inna@gmail.com>
2022-09-09 16:04:06 +0300

    Normalize pressure data. Make pressure units conversion

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -5,6 +5,10 @@ function findMeasurement(measurements, code) {
   return measurements.find(p => p.phenomenon === code)?.value;
 }
 
+function pa2mmhg(p) {
+  return p ? (p / 133.322).toFixed() : undefined;
+}
+
 function Data({ data }) {
   return (
     <div className="data">
@@ -16,22 +20,22 @@ function Data({ data }) {
             <th>Coordinates</th>
             <th>PM2.5, ug/m3</th>
             <th>PM10, ug/m3</th>
-            <th>Temperature, Celcius</th>
+            <th>Temperature, C</th>
             <th>Humidity, %</th>
-            <th>Pressure, hPa FIX ME!!!</th>
+            <th>Pressure, mmHg</th>
           </tr>
         </thead>
         <tbody>
           {data.map(s => (
             <tr key={s.id}>
-              <td>{s.cityName}</td>
+              <td>{s.cityName} {s.id}</td>
               <td>{s.stationName}</td>
               <td>{s.latitude}, {s.longitude}</td>
               <td>{findMeasurement(s.latestMeasurements, "pm25")}</td>
               <td>{findMeasurement(s.latestMeasurements, "pm10")}</td>
               <td>{findMeasurement(s.latestMeasurements, "temperature")}</td>
               <td>{findMeasurement(s.latestMeasurements, "humidity")}</td>
-              <td>{findMeasurement(s.latestMeasurements, "pressure_pa")}</td>
+              <td>{pa2mmhg(findMeasurement(s.latestMeasurements, "pressure_pa"))}</td>
             </tr>
           ))}
         </tbody>
