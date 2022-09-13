c1d69baab1c4ba81b79ebba75966f2049b97f5b6
Inna Huzhva <guzhva.inna@gmail.com>
2022-09-13 20:24:30 +0300

    Implement loading of station data

--- src/data/StationData.jsx
+++ src/data/StationData.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link, useParams } from 'react-router-dom';
+import { useStationData } from "./useData.jsx";
 
 function StationData() {
   const { stationId } = useParams();
+  const data = useStationData(stationId);
   return (
     <div>
       <div>
         <Link to="..">Back</Link>
       </div>
       <div>
-        Station {stationId}
+        {data.status === "loading" && <h5>Loading...</h5>}
+        {data.status === "error" && (
+          <h1 style={{ color: "maroon" }}>
+            Error happened when loading station data! {data.error}
+          </h1>
+        )}
+        {data.status === "success" && (
+          <div>
+            {JSON.stringify(data.data)}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/data/useData.jsx
+++ src/data/useData.jsx
@@ -18,3 +18,22 @@ export function useData() {
 
   return data;
 }
+
+export function useStationData(stationId) {
+  const [data, setData] = useState({ status: "loading" });
+
+  useEffect(() => {
+    fetch(`/api/history/${stationId}`)
+      .then((response) => {
+        if (!response.ok) {
+          setData({ status: "error", error: response.statusText });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData({ status: "success", data });
+      });
+  }, []);
+
+  return data;
+}
