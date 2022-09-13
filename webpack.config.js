f240bd4459d50df5127ece8d97cb990ce3769c33
Inna Huzhva <guzhva.inna@gmail.com>
2022-09-14 00:17:33 +0300

    Copy static files to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -36,6 +37,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       publicPath: "/",
+    }),
+    new CopyPlugin({
+      patterns: [
+        "public",
+      ],
     }),
   ],
   devServer: {
